37b675d25ec1edfa96199acd59f5bdc1b94184cc
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-12-23 18:49:17 +0100

    bug fixes
    Co-Authored-By: annasanchez27 <43809690+annasanchez27@users.noreply.github.com>

--- model2.py
+++ model2.py
@@ -25,30 +25,32 @@ class DCGenerator2(tfkl.Layer):
 
         # based on: https://github.com/pytorch/examples/blob/master/dcgan/main.py
         self.netG = tf.keras.Sequential([
-            tfkl.Conv2DTranspose(self.ngf * 8, kernel_size=4, stride=1, padding='valid', bias=False),
+            tfkl.Conv2DTranspose(self.ngf * 8, kernel_size=4, strides=1, padding='valid', use_bias=False),
             tfkl.BatchNormalization(),
             tfkl.ReLU(),
             # state size. (ngf*8) x 4 x 4
-            tfkl.Conv2DTranspose(self.ngf * 4, kernel_size=4, stride=2, padding='same', bias=False),
+            tfkl.Conv2DTranspose(self.ngf * 4, kernel_size=4, strides=2, padding='same', use_bias=False),
             tfkl.BatchNormalization(),
             tfkl.ReLU(),
             # state size. (ngf*4) x 8 x 8
-            tfkl.Conv2DTranspose(self.ngf * 2, kernel_size=4, stride=2, padding='same', bias=False),
+            tfkl.Conv2DTranspose(self.ngf * 2, kernel_size=4, strides=2, padding='same', use_bias=False),
             tfkl.BatchNormalization(),
             tfkl.ReLU(),
             # state size. (ngf*2) x 16 x 16
-            tfkl.Conv2DTranspose(self.ngf, kernel_size=4, stride=2, padding='same', bias=False),
+            tfkl.Conv2DTranspose(self.ngf, kernel_size=4, strides=2, padding='same', use_bias=False),
             tfkl.BatchNormalization(),
             tfkl.ReLU(),
             # state size. (ngf) x 32 x 32
-            tfkl.Conv2DTranspose(self.num_channels, kernel_size=4, stride=2, padding='same', bias=False),
+            tfkl.Conv2DTranspose(self.num_channels, kernel_size=4, strides=2, padding='same', use_bias=False),
             tfkl.Activation(tfa.tanh)
             # state size. (num_channels) x 64 x 64
         ]
         )
 
     def call(self, z, embed):
-        projected_embed = self.projection(embed).unsqueeze(2).unsqueeze(3)
+        projected_embed = self.projection(embed)
+        projected_embed = tf.expand_dims(tf.expand_dims(projected_embed, 2), 3)
+        z = tf.expand_dims(tf.expand_dims(z, 2), 3)
         latent_vector = tf.concat([projected_embed, z], 1)
         output = self.netG(latent_vector)
         return output
@@ -61,13 +63,15 @@ class Concat_embed(tfkl.Layer):
             tfkl.Dense(projected_embed_dim),
             tfkl.BatchNormalization(),
             tfkl.LeakyReLU(0.2)
-        ]
-        )
+        ])
 
     def call(self, inp, embed):
+        print(f"input shape: {inp.shape}, embed shape: {embed.shape}")
         projected_embed = self.projection(embed)
-        replicated_embed = projected_embed.repeat(4, 4, 1, 1).permute(2, 3, 0, 1)
-        hidden_concat = tf.concat([inp, replicated_embed], 1)
+        embed = tf.expand_dims(tf.expand_dims(projected_embed, 1), 1)
+        embed = tf.tile(embed, [1, 4, 4, 1])
+        hidden_concat = tf.concat([inp, embed], axis=3)
+        print(f"hidden_concat shape: {hidden_concat.shape}")
         return hidden_concat
 
 
@@ -84,43 +88,42 @@ class DCDiscriminator2(tfkl.Layer):
 
         self.netD_1 = tf.keras.Sequential([
             # input is (nc) x 64 x 64
-            tfkl.Conv2D(self.ndf, 4, 2, padding='same', bias=False),
+            tfkl.Conv2D(self.ndf, 4, 2, padding='same', use_bias=False),
             tfkl.LeakyReLU(0.2),
             # state size. (ndf) x 32 x 32
-            tfkl.Conv2D(self.ndf * 2, 4, 2, padding='same', bias=False),
+            tfkl.Conv2D(self.ndf * 2, 4, 2, padding='same', use_bias=False),
             tfkl.BatchNormalization(),
             tfkl.LeakyReLU(0.2),
             # state size. (ndf*2) x 16 x 16
-            tfkl.Conv2D(self.ndf * 4, 4, 2, padding='same', bias=False),
+            tfkl.Conv2D(self.ndf * 4, 4, 2, padding='same', use_bias=False),
             tfkl.BatchNormalization(),
             tfkl.LeakyReLU(0.2),
             # state size. (ndf*4) x 8 x 8
-            tfkl.Conv2D(self.ndf * 8, 4, 2, padding='same', bias=False),
+            tfkl.Conv2D(self.ndf * 8, 4, 2, padding='same', use_bias=False),
             tfkl.BatchNormalization(),
             tfkl.LeakyReLU(0.2),
         ])
 
         self.projector = Concat_embed(self.embed_dim, self.projected_embed_dim)
 
-        self.netD_2 = tf.keras.Sequential(
+        self.netD_2 = tf.keras.Sequential([
             # state size. (ndf*8) x 4 x 4
-            tfkl.Conv2D(1, 4, 1, padding='valid', bias=False),
+            tfkl.Conv2D(1, 4, 1, padding='valid', use_bias=False),
             tfkl.Activation(tfa.sigmoid)
-        )
+        ])
 
-
-def call(self, x, embed):
-    x_intermediate = self.netD_1(x)
-    x = self.projector(x_intermediate, embed)
-    x = self.netD_2(x)
-    return x, x_intermediate
+    def call(self, x, embed):
+        x_intermediate = self.netD_1(x)
+        x = self.projector(x_intermediate, embed)
+        x = self.netD_2(x)
+        return x, x_intermediate
 
 
 class GAN2(tf.keras.Model):
     def __init__(self, config):
         super(GAN2, self).__init__()
 
-        self.noise_dim = 100
+        self.noise_dim = 128
         self.batch_size = config['batch_size']
 
         self.generator = DCGenerator2()
@@ -211,7 +214,7 @@ class GAN2(tf.keras.Model):
 
         return discriminator_loss, generator_loss
 
-    def call(self, x, embed, wrong_images, train_step_new=True):
+    def call(self, x, embed, wrong_images, train_step_new=False):
         if train_step_new:
             return self.train_step_new(x, embed, wrong_images)
         return self.train_step(x, embed, wrong_images)
